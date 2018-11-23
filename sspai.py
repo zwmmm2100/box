0985dcef45fdf41a56e923142875584bf3162fe2
zwmmm2100 <zwmmm2100@gmail.com>
2018-11-23 20:01:53 +0800

    2

--- sspai.py
+++ sspai.py
@@ -11,18 +11,24 @@ headers={
             'Connection':'keep-alive'}
 xurl = 'https://sspai.com/api/v1/articles?offset=0&limit=200&type=recommend_to_home&sort=recommend_to_home_at&include_total=false'
 
-#根据链接收集所有的id加到列表里面
+#定义函数根据链接收集所有的id加到列表里面
 def num_lists(url):
 	driver = requests.get(url)
+	#根据url返回requeson对象
 	tesss = driver.json()
+	#用json方法把requson对象变成字典
 	lists = tesss["list"]
+	#取出list列表
 	urls = []
+	#创建放置id的列表
+	#用循环语句取出列表中的各个对象的id并添加到urls列表中
+	#并把这个urls列表返回函数
 	for i in lists:
 		
 		urls.append(i['id'])
 		print(i['id'])
 	return urls
-#根据id列表拼凑real链接地址
+#根据id列表拼凑real链接地址并添加到real_url_lists列表中
 def url_lists(url_l):
 	real_url_lists = []
 	for i in url_l:
@@ -37,6 +43,7 @@ def down_html(lists):
 		url_text = res.text
 		soup = BeautifulSoup(url_text,'lxml')
 		ssss= soup.find(class_="article-margin")
+		#取出文章标题
 		title = soup.find(class_="article-margin").find(class_="title").text
 		html_name = 'C:/Users/Administrator/Desktop/html/' + str(lists.index(i)) + '.html'
 		s = codecs.open(html_name,'w',encoding='utf-8')
@@ -44,6 +51,7 @@ def down_html(lists):
 		s.close()
 
 		title_url = 'file:///C:/Users/Administrator/Desktop/html/' + str(lists.index(i)) + '.html'
+		#把文章标题和文章本地地址当做键值对共同添加到字典中
 		title_files[title] = title_url
 		print('[' + str(lists.index(i)+1) + '/200]' + title + ' is ok.')
 		time.sleep(10)
@@ -57,6 +65,7 @@ oo = down_html(lists)
 print(oo)
 
 doc = '<head><meta charset="UTF-8"></head>'
+#拼凑成文章目录
 for key in oo:
 	doc = doc + "<a href=%s><p>%s</p></a>" %(oo[key],key) + '\n'
 list_name = 'C:/Users/Administrator/Desktop/html/' + 'MULU.html'
