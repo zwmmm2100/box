a622c91a184c32bf7f1bb44ea4d2f3643330167e
zwmmm2100 <zwmmm2100@g,mail.com>
2018-11-26 16:47:28 +0800

    '222'

---
+++ haizeiwang.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+
+url = 'https://www.fzdm.com/manhua/002/Vol_001/'
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'}
+session = HTMLSession()
+r = session.get(url)
+
+print(r.html.links)
